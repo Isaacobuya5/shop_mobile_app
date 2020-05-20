f494cfcd136d465c200fb4d487d12feeb675a111
Isaac Obuya <Szack4477@gmail.com>
2020-05-20 07:55:44 +0300

    set up a listener in product detail screen to get a particular product and display its title in the appBar

--- lib/screens/ProductDetails.dart
+++ lib/screens/ProductDetails.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/products.dart';
 
 class ProductDetail extends StatelessWidget {
 
@@ -9,9 +11,10 @@ class ProductDetail extends StatelessWidget {
     // id for this particular product
     final productId = ModalRoute.of(context).settings.arguments as String;
     // retrieve value for this particular product
+    final loadedProduct = Provider.of<ProductsProvider>(context).items.firstWhere((product) => product.id == productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
