f84b98ae780c48747ac19d6b1e82c96fea0a7a32
Isaac Obuya <Szack4477@gmail.com>
2020-06-12 12:04:11 +0300

    implementing optimistic delete for products

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -163,9 +163,30 @@ class ProductsProvider with ChangeNotifier {
   }
 
   // method to delete a product
-  void deleteProduct(String id) {
+  void deleteProduct(String id) async {
+    final url = 'https://shop-mobile-app-3f890.firebaseio.com/products/$productId.json';
+    // get this products index
+    final existingProductIndex = _items.indexWhere((product) => product.id == id);
+    // store the reference to this product
+    var existingProduct = _items[existingProductIndex];
     // delete a product with the given id
-    _items.removeWhere((product) => product.id == id);
-    notifyListeners();
+    // _items.removeWhere((product) => product.id == id);
+    _items.removeAt(existingProductIndex);
+    // delete this product from server
+    http.delete(url).then((response) {
+      // delete requests normally do not throw error for status code greater than 400
+      // thus need to provide our own custom error handling
+      if (response.statusCode >= 400) {
+        
+      }
+      // if delete succesful then clear the existing product from memory
+      existingProduct = null;
+        notifyListeners();
+    }).catchError((_) {
+      // if error occurs, we need to return the deleted product to the list of products
+      _items.insert(existingProductIndex, existingProduct);
+        notifyListeners();
+    });
+  
   }
 }
